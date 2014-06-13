922a188f3dbf1b324bdb3cd593f0b73caa0b3d1d
Gabriel Osorio <gabriel.osorio@neonroots.com>
2014-06-13 11:01:57 -0300

    Update foo bar resource with a dummy user.

--- main.rb
+++ main.rb
@@ -7,7 +7,17 @@ module Dummy
     resource :foo do
       desc 'Return a dummy JSON response'
       get :bar do
-        { foo: 'bar' }
+        {
+          id: "someID",
+          first_name: "John",
+          last_name: "Doe",
+          gender: "male",
+          email: "john@doe.com",
+          timezone: "-3",
+          locale: 'en_US',
+          image_url: 'http://www.thesquashedtomato.co.za/wp-content/uploads/2012/01/facebook-profile-picture-no-pic-avatar.jpg',
+          birthday: Time.now
+        }
       end
     end
   end
