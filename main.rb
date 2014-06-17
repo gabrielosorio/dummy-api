8cec12285c3d0ac8df37501c396c881739af2f07
Gabriel Osorio <gabriel.osorio@neonroots.com>
2014-06-17 16:37:58 -0300

    Add dummy user details resource.

--- main.rb
+++ main.rb
@@ -3,21 +3,28 @@ require 'grape'
 module Dummy
   class API < Grape::API
     format :json
+    prefix 'api'
 
-    resource :foo do
-      desc 'Return a dummy JSON response'
-      get :bar do
-        {
-          id: "someID",
-          first_name: "John",
-          last_name: "Doe",
-          gender: "male",
-          email: "john@doe.com",
-          timezone: "-3",
-          locale: 'en_US',
-          image_url: 'http://www.thesquashedtomato.co.za/wp-content/uploads/2012/01/facebook-profile-picture-no-pic-avatar.jpg',
-          birthday: Time.now
-        }
+    resource :users do
+      desc 'Return a dummy user'
+      params do
+        requires :facebook_id, type: String, desc: 'Facebook ID'
+      end
+      route_param :facebook_id do
+        get do
+          {
+            id: 'someID',
+            facebook_id: params[:facebook_id],
+            first_name: 'John',
+            last_name: 'Doe',
+            gender: 'male',
+            email: 'john@doe.com',
+            timezone: '-3',
+            image_url: 'http://www.thesquashedtomato.co.za/wp-content/uploads/2012/01/facebook-profile-picture-no-pic-avatar.jpg',
+            birthday: Time.now,
+            religion: 'budist'
+          }
+        end
       end
     end
   end
